faa09f366c8a675febed2331cd92075ca903afb4
Jose Labra <jelabra@gmail.com>
2020-04-08 08:39:10 +0200

    Filled with build

--- README.md
+++
@@ -1,7 +0,0 @@
-# wikishape
-
-Wikidata and wikibase description and validation tool using Shapes.
-
-This is a [React](https://reactjs.org/) application currently deployed at [http://wikishape.weso.es/](http://wikishape.weso.es/)
-
-[![DOI](https://zenodo.org/badge/DOI/10.5281/zenodo.3551126.svg)](https://doi.org/10.5281/zenodo.3551126)
---
+++ precache-manifest.52c52d8929b0085e05a898af6cc6c385.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c12c3b898b02bff78cf7bd8390c4f481",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9d8038135e48136b0a9d",
+    "url": "/static/css/2.51c9cb36.chunk.css"
+  },
+  {
+    "revision": "f69f3d3f2ce73c75fe3a",
+    "url": "/static/css/main.85b607a5.chunk.css"
+  },
+  {
+    "revision": "9d8038135e48136b0a9d",
+    "url": "/static/js/2.a529cfc0.chunk.js"
+  },
+  {
+    "revision": "f69f3d3f2ce73c75fe3a",
+    "url": "/static/js/main.13c900e5.chunk.js"
+  },
+  {
+    "revision": "9c9ec91c21283e72e551",
+    "url": "/static/js/runtime~main.098997a1.js"
+  },
+  {
+    "revision": "09ee6d5ff58e2c3fa63aa02c6eacdf1d",
+    "url": "/static/media/TurtleForm.09ee6d5f.delete"
+  }
+]);
